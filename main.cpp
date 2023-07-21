802c78f738838b73fae8d34100287cb36a82c585
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2023-07-21 14:14:53 +0900

    MT3_04_00

--- main.cpp
+++ main.cpp
@@ -23,6 +23,26 @@ const int kWindowHeight = 720;
 
 const float kSpeed = 0.05f;
 
+const float deltaTime = 1.0f/60.0f;
+
+struct Ball
+{
+	Vector3 position;
+	Vector3 velocity;
+	Vector3 acceleration;
+	float mass;//質量kg
+	float radius;
+	unsigned int color;
+};
+
+struct Spring 
+{
+	Vector3 anchor;
+	float naturalLength;
+	float stiffness;
+	float dampingCoefficient;
+};
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -30,19 +50,37 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
-	Vector3 a{ 0.2f,1.0f,0.0f }, b{2.4f,3.1f,1.2f};
-	Vector3 c = a + b;
-	Vector3 d = a - b;
-	Vector3 e = a + 2.4f;
-	Vector3 rotate{0.4f,1.43f,-0.8f};
-	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
-	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
-	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
-	Matrix4x4 rotateMatrix = rotateXMatrix * rotateYMatrix * rotateZMatrix;
-	//Sphere sphere{ {1.0f,0.0f,0.0f},1.0f };
+	//カメラ操作用
+	/*int LeftClick = 0;
+	int preLeftClick=0;
+	Vector2 mousePosition;
+	Vector2 preMousePosition;
+	*/
+
+	Vector3 cameraTranslate{ 0.0f,1.9f,-6.49f };
+	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };
+	DebugCamera debugCamera;
+	debugCamera.Initialize({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+
+	bool Calclation = false;
+	
+	Spring spring;
+	spring.anchor = {0.0f,0.0f,0.0f};
+	spring.naturalLength = 1.0f;
+	spring.stiffness = 100.0f;
+	spring.dampingCoefficient = 2.0f;
+
+	Ball ball;
+	ball.position = {1.2f,0.0f,0.0f};
+	ball.velocity = {0.0f,0.0f,0.0f};
+	ball.mass = 2.0f;
+	ball.radius = 0.05f;
+	ball.color = BLUE;
+
+		//Sphere sphere{ {1.0f,0.0f,0.0f},1.0f };
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -55,18 +93,46 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-		
-		
+		debugCamera.Update();
+
+
+
+		//Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate,cameraTranslate);
+		Matrix4x4 viewMatrix = Inverse(debugCamera.GetWorld());
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+
+		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+
+		Matrix4x4 viewPortMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+
+		if (Calclation)
+		{
+			Vector3 diff = ball.position - spring.anchor;
+			float length = Length(diff);
+			if (length != 0.0f)
+			{
+				Vector3 direction = Normalize(diff);
+				Vector3 restPosition = spring.anchor + direction * spring.naturalLength;
+				Vector3 displacement = length * (ball.position - restPosition);
+				Vector3 restoringForce = -spring.stiffness * displacement;
+				Vector3 dampingForce = -spring.dampingCoefficient * ball.velocity;
+				Vector3 force = restoringForce + dampingForce;
+				ball.acceleration = force / ball.mass;
+			}
+
+			ball.velocity += ball.acceleration * deltaTime;
+			ball.position += ball.velocity * deltaTime;
+		}
+
+
+
+
 		ImGui::Begin("Window");
-		//ImGui::DragFloat3("CameraTranslate",&cameraTranslate.x,0.01f);
-		ImGui::Text("c:%f, %f, %f", c.x, c.y, c.z);
-		ImGui::Text("d:%f, %f, %f", d.x, d.y, d.z);
-		ImGui::Text("e:%f, %f, %f", e.x, e.y, e.z);
-		ImGui::Text("matrix:\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n%f, %f, %f, %f\n",
-			rotateMatrix.m[0][0], rotateMatrix.m[0][1], rotateMatrix.m[0][2], rotateMatrix.m[0][3],
-			rotateMatrix.m[1][0], rotateMatrix.m[1][1], rotateMatrix.m[1][2], rotateMatrix.m[1][3], 
-			rotateMatrix.m[2][0], rotateMatrix.m[2][1], rotateMatrix.m[2][2], rotateMatrix.m[2][3], 
-			rotateMatrix.m[3][0], rotateMatrix.m[3][1], rotateMatrix.m[3][2], rotateMatrix.m[3][3]);
+		if (ImGui::Button("Start"))
+		{
+			Calclation = true;
+		}
 		ImGui::End();
 
 
@@ -77,8 +143,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		
-		
+
+		DrawGrid(viewProjectionMatrix, viewPortMatrix);
+		Segment segment{spring.anchor,ball.position-spring.anchor};
+		DrawSegment(segment,WHITE, viewProjectionMatrix, viewPortMatrix);
+		Sphere sphere{ ball.position,ball.radius };
+		DrawSphere(sphere, viewProjectionMatrix, viewPortMatrix,ball.color);
 		///
 		/// ↑描画処理ここまで
 		///
